b1264570609404ca6db1cb9a19ecf93943ea1cfa
G-GitCoder <Jacob0418@163.com>
2020-08-20 22:47:54 +0800

    添加文件、修改文件

--- day04-code/src/cn/itcast/day04/demo01/HelloWorld.java
+++ day04-code/src/cn/itcast/day04/demo01/HelloWorld.java
@@ -3,5 +3,6 @@ package cn.itcast.day04.demo01;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
---
+++ day04-code/src/cn/itcast/day04/demo02/Demo01Method.java
@@ -0,0 +1,7 @@
+package cn.itcast.day04.demo02;
+
+public class Demo01Method {
+    public static void main(String[] args) {
+        System.out.println("Git GitHub IDEA");
+    }
+}
