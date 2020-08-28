62e7debf960b95c78d039ebc4f50fdf325a381b0
G-GitCoder <Jacob0418@163.com>
2020-08-28 08:59:59 +0800

    方法重载练习

--- day04-code/src/cn/itcast/day04/demo04/Demo01MethodOverload.java
+++ day04-code/src/cn/itcast/day04/demo04/Demo01MethodOverload.java
@@ -4,7 +4,7 @@ package cn.itcast.day04.demo04;
 对于功能类似的方法来说，因为参数列表不一样，却要记住很多方法名称，太麻烦
 
 方法的重载（Overload）：多个方法的名称一样，但是参数列表不一样
-好处：只需要机主唯一一个方法名称，就可以实现类似的多个功能
+好处：只需要记住唯一一个方法名称，就可以实现类似的多个功能
 
 方法的重载与下列因素相关：
 1.参数个数不同
@@ -21,23 +21,48 @@ public class Demo01MethodOverload {
         System.out.println(sumThree(10,20,30));//40
         System.out.println(sumFour(10,20,30,40));//100*/
 
-        System.out.println(sum(10,20));//两个参数的方法
-        System.out.println(sum(10,20,30));//三个参数的方法
-        System.out.println(sum(10,20,30,40));//四个参数的方法
+        System.out.println(sum(10, 20));//两个参数的方法
+        System.out.println(sum(10, 20, 30));//三个参数的方法
+        System.out.println(sum(10, 20, 30, 40));//四个参数的方法
 //        System.out.println(sum(10,20,30,40,50));//找不到匹配的方法，错误
     }
 
-    public static int sum(int a, int b){
+    //错误写法！与参数的名称无关
+//    public static int sum(int x, int y){
+//        return x + y;
+//    }
+
+    //错误写法！与方法的返回值类型无关
+//    public static double sum(int a, int b){
+//        return a + b + 0.0;
+//    }
+
+    //方法重载：参数的多类型顺序不同
+    public static int sum(int a, double b) {
+        return (int) (a + b);
+    }
+
+    public static int sum(double a, int b) {
+        return (int) (a + b);
+    }
+
+    public static int sum(int a, int b) {
         System.out.println("有2个参数的方法");
         return a + b;
     }
 
-    public static int sum(int a, int b, int c){
+    //方法重载：参数类型不同
+    public static int sum(double a, double b) {
+        return (int) (a + b);
+    }
+
+    //方法重载：参数个数不同
+    public static int sum(int a, int b, int c) {
         System.out.println("有3个参数的方法");
         return a + b + c;
     }
 
-    public static int sum(int a, int b,int c, int d){
+    public static int sum(int a, int b, int c, int d) {
         System.out.println("有4个参数的方法");
         return a + b + c + d;
     }
