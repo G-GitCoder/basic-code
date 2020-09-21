66b885be60479e9aced73f6c4962e69e29046fb4
G-GitCoder <Jacob0418@163.com>
2020-09-21 22:11:45 +0800

    private关键字

---
+++ day06-code/src/cn/itcast/day06/demo03/Demo04Student.java
@@ -0,0 +1,15 @@
+package cn.itcast.day06.demo03;
+
+public class Demo04Student {
+    public static void main(String[] args) {
+        Student stu = new Student();
+
+        stu.setAge(20);
+        stu.setMale(true);
+        stu.setName("鹿晗");
+
+        System.out.println("姓名：" + stu.getName());
+        System.out.println("年龄：" + stu.getAge());
+        System.out.println("爷们？" + stu.isMale());
+    }
+}
---
+++ day06-code/src/cn/itcast/day06/demo03/Student.java
@@ -0,0 +1,36 @@
+package cn.itcast.day06.demo03;
+
+/*
+对于基本数据类型中的boolean值，Getter方法一定要写成isXxx的形式，而setXxx保持不变
+* */
+public class Student {
+
+    private String name;//姓名
+    private int age;//年龄
+    private boolean male;//性别
+
+    public void setMale(boolean b) {
+        male = b;
+    }
+
+    public boolean isMale() {
+        return male;
+    }
+
+    public void setName(String str) {
+        name = str;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int num) {
+        age = num;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
