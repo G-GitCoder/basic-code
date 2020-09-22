2e271e9aa59e4bd23925ee02335f60f3d388c479
G-GitCoder <Jacob0418@163.com>
2020-09-22 18:40:20 +0800

    构造方法

---
+++ day06-code/src/cn/itcast/day06/demo04/Demo02Student.java
@@ -0,0 +1,16 @@
+package cn.itcast.day06.demo04;
+
+public class Demo02Student {
+
+    public static void main(String[] args) {
+        Student stu1 = new Student();//无参构造
+        System.out.println("=====================");
+
+        Student stu2 = new Student("王思聪",30);//全参构造
+        System.out.println("姓名：" + stu2.getName() + "，年龄：" + stu2.getAge());
+
+        //如果需要改变对象当中的成员变量的值，仍然还需要使用setXxx方法
+        stu2.setAge(40);
+        System.out.println("姓名：" + stu2.getName() + "，年龄：" + stu2.getAge());
+    }
+}
