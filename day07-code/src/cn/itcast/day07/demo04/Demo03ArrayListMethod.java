e1dbb64f25158626dc574fe4c13f32670b31e4f8
G-GitCoder <Jacob0418@163.com>
2020-10-18 18:02:52 +0800

    ArrayList应用

--- day07-code/src/cn/itcast/day07/demo04/Demo03ArrayListMethod.java
+++ day07-code/src/cn/itcast/day07/demo04/Demo03ArrayListMethod.java
@@ -1,5 +1,7 @@
 package cn.itcast.day07.demo04;
 
+import java.util.ArrayList;
+
 /*
 ArrayList当中的常用方法有：
 
@@ -18,4 +20,33 @@ public int size()
 获取集合的尺寸长度，返回值是集合当中的元素个数
 * */
 public class Demo03ArrayListMethod {
+
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        System.out.println(list);//[]
+
+        //向集合当中添加元素：add
+        boolean success = list.add("柳岩");
+        System.out.println(list);//[柳岩]
+        System.out.println("添加的动作是否成功：" + success);//true
+
+        list.add("高圆圆");
+        list.add("赵又廷");
+        list.add("李小璐");
+        list.add("贾乃亮");
+        System.out.println(list);//[柳岩, 高圆圆, 赵又廷, 李小璐, 贾乃亮]
+
+        //从集合当中获取元素：get，索引值从0开始
+        String name = list.get(2);
+        System.out.println("第2号索引位置：" + name);//赵又廷
+
+        //从集合当中删除元素：remove，索引值从0开始
+        String whoRemoved = list.remove(3);
+        System.out.println("被删除的人是：" + whoRemoved);//李小璐
+        System.out.println(list);//[柳岩, 高圆圆, 赵又廷, 贾乃亮]
+
+        //获取集合的长度尺寸，也就是其中元素的个数
+        int size = list.size();
+        System.out.println("集合的长度是：" + size);
+    }
 }
