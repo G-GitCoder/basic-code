8c86ff8f5ed10363ab8c03146e037f53d4b6097c
G-GitCoder <Jacob0418@163.com>
2020-10-18 11:35:20 +0800

    这俩文件真恶心

--- day06-code/src/cn/itcast/day06/demo03/Student.java
+++ day06-code/src/cn/itcast/day06/demo03/Student.java
@@ -7,30 +7,29 @@ public class Student {
 
     private String name;//姓名
     private int age;//年龄
-    private boolean male;//性别
+    private boolean male;//性别，爷们？
 
-    public void setMale(boolean b) {
-        male = b;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 
     public boolean isMale() {
         return male;
     }
 
-    public void setName(String str) {
-        name = str;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setAge(int num) {
-        age = num;
-    }
-
-    public int getAge() {
-        return age;
+    public void setMale(boolean male) {
+        this.male = male;
     }
 }
-
