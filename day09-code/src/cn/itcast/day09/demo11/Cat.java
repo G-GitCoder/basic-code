6336cd57566dd3127750dabd6c43a63ee7db237f
G-GitCoder <Jacob0418@163.com>
2020-12-09 08:58:21 +0800

    抽象类和抽象方法

---
+++ day09-code/src/cn/itcast/day09/demo11/Cat.java
@@ -0,0 +1,9 @@
+package cn.itcast.day09.demo11;
+
+public class Cat extends Animal {
+
+    @Override
+    public void eat() {
+        System.out.println("猫吃鱼");
+    }
+}
---
+++ day09-code/src/cn/itcast/day09/demo13/Animal.java
@@ -0,0 +1,8 @@
+package cn.itcast.day09.demo13;
+
+//最高的抽象父类
+public abstract class Animal {
+
+    public abstract void sleep();
+    public abstract void eat();
+}
