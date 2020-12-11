5227f2ed90251e4214b1ec864594a9b751653ef7
G-GitCoder <Jacob0418@163.com>
2020-12-11 09:04:47 +0800

    发红包案例实现

---
+++ day09-code/src/cn/itcast/day09/demo14/MainRedPacket.java
@@ -0,0 +1,33 @@
+package cn.itcast.day09.demo14;
+
+import java.util.ArrayList;
+
+public class MainRedPacket {
+
+    public static void main(String[] args) {
+        Manager manager = new Manager("群主",100);
+
+        Member one = new Member("成员A",0);
+        Member two = new Member("成员B",0);
+        Member three = new Member("成员C",0);
+
+        manager.show();
+        one.show();
+        two.show();
+        three.show();
+        System.out.println("===================");
+
+        //群主总共发20块钱，分成3个红包
+        ArrayList<Integer> redList = manager.send(20,3);
+        //三个普通成员收红包
+        one.receive(redList);
+        two.receive(redList);
+        three.receive(redList);
+
+        manager.show();//80
+        //6、6、8 随机分给三个人
+        one.show();
+        two.show();
+        three.show();
+    }
+}
