c2525e6115e4334f34439fb1b4d614cad58b76b9
G-GitCoder <Jacob0418@163.com>
2021-01-18 12:37:38 +0800

    接口的使用【2021.1.18.开始每天努力学习】

---
+++ day10-code/src/cn/itcast/day10/demo01/MyInterfaceDefaultA.java
@@ -0,0 +1,9 @@
+package cn.itcast.day10.demo01;
+
+public class MyInterfaceDefaultA implements MyInterfaceDefault {
+
+    @Override
+    public void methodAbs() {
+        System.out.println("实现了抽象方法，AAA");
+    }
+}
