0cedc1732d1578171912529dad720242e28ef135
G-GitCoder <Jacob0418@163.com>
2021-01-27 19:58:29 +0800

    接口各类方法使用【2021.1.27.开始每天努力学习】

---
+++ day10-code/src/cn/itcast/day10/demo03/MyInterfaceB.java
@@ -0,0 +1,12 @@
+package cn.itcast.day10.demo03;
+
+public interface MyInterfaceB {
+
+    public abstract void methodB();
+
+    public abstract void methodCommon();
+
+    public default void methodDefault() {
+        System.out.println("BBB");
+    }
+}
